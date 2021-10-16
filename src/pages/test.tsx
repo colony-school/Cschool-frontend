96e1eec5646e99a3e68307dbde169be96ad9108b
SiravitPhokeed <siravitsasa@gmail.com>
2021-10-16 18:22:40 +0700

    Imported Dropdown SCSS from Colony

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -13,11 +13,13 @@ import styles from "../styles/pages/test.module.scss";
 const Test: NextPage = () => {
     return (
         <main>
-            <Dropdown list={[
-                {id: 0, text: "Show"},
-                {id: 1, text: "Censored before show"},
-                {id: 2, text: "Don't show"}
-            ]} startsAs={0} onChange={() => console.log("hi")} />
+            <div className={styles["dropdown"]}>
+                <Dropdown list={[
+                    {id: 0, text: "Show"},
+                    {id: 1, text: "Censored before show"},
+                    {id: 2, text: "Don't show"}
+                ]} startsAs={0} onChange={() => console.log("hi")} />
+            </div>
         </main>
     );
 }
