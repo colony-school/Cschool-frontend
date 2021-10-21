a43580462658b783435552b6479193aa47d07eba
SiravitPhokeed <siravitsasa@gmail.com>
2021-10-21 17:51:45 +0700

    Menu translation file (not used yet)

---
+++ utils/translations/menu.ts
@@ -0,0 +1,15 @@
+export const en = {
+    "dashboard": "Dashboard",
+    "events": "Events",
+    "schedule": "Schedule",
+    "todo": "To-do",
+    "classrooms": "Classrooms",
+}
+
+export const th = {
+    "dashboard": "หน้าหลัก",
+    "events": "อีเวนท์",
+    "schedule": "ตารางเรียน",
+    "todo": "งาน",
+    "classrooms": "ห้องเรียน",
+}
