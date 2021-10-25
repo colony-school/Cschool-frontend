bbe88cbe08b32dc80b412dfe0aa8b081c154f91a
SiravitPhokeed <siravitsasa@gmail.com>
2021-10-25 18:30:35 +0700

    Framer Motion dropdown

--- src/components/form/dropdown.tsx
+++ src/components/form/dropdown.tsx
@@ -1,5 +1,7 @@
 // External modules import
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { popDown } from "../../../utils/animations/global";
 
 // Style sheets import
 import styles from "../../styles/components/dropdown.module.scss";
@@ -11,7 +13,7 @@ type ListItem = {
 }
 
 // Main component
-interface DropdownProps {
+type DropdownProps = {
     list: Array<ListItem>,
     startsAs: number,
     onChange: Function
@@ -27,7 +29,7 @@ export const Dropdown = ({ list, startsAs, onChange }: DropdownProps): JSX.Eleme
     const [droppedDown, setDroppedDown] = useState(false);
     const [current, setCurrent] = useState(list.filter((item) => item.id === startsAs)[0]);
 
-    function renderCurrent(current: ListItem) {
+    const DropDownTrigger = ({ current }: { current: ListItem }) => {
         return (
             <button className={styles["dropdown-trigger"]} onClick={() => setDroppedDown(!droppedDown)}>
                 <span>{current.text}</span>
@@ -35,22 +37,27 @@ export const Dropdown = ({ list, startsAs, onChange }: DropdownProps): JSX.Eleme
         );
     }
 
-    function renderDropdown(list: Array<ListItem>) {
-        if (droppedDown)
-            return (
-                <div className={styles["dropdown"]}>
-                    <div className={styles["dropdown-scroll-area"]}>
-                        {list.map((item) => {
-                            return (
-                                <button key={item.id} className={styles[`option-${(item.id === current.id)}`]}
-                                    onClick={() => handleChange(item)}>
-                                    <span>{item.text}</span>
-                                </button>
-                            );
-                        })}
-                    </div>
+    const DropdownContent = ({ list }: { list: Array<ListItem> }): JSX.Element => {
+        return (
+            <motion.div
+                initial="hidden"
+                animate="enter"
+                exit="exit"
+                variants={popDown}
+                transition={{ duration: 0.2, ease: "easeOut" }}
+                className={styles["dropdown"]}>
+                <div className={styles["dropdown-scroll-area"]}>
+                    {list.map((item) => {
+                        return (
+                            <button key={item.id} className={styles[`option-${(item.id === current.id)}`]}
+                                onClick={() => handleChange(item)}>
+                                <span>{item.text}</span>
+                            </button>
+                        );
+                    })}
                 </div>
-            );
+            </motion.div>
+        );
     }
 
     function handleChange(event: ListItem) {
@@ -61,8 +68,10 @@ export const Dropdown = ({ list, startsAs, onChange }: DropdownProps): JSX.Eleme
     
     return (
         <>
-            {renderCurrent(current)}
-            {renderDropdown(list)}
+            <DropDownTrigger current={current} />
+            <AnimatePresence>
+                {droppedDown ? <DropdownContent list={list} key="dropdown" /> : null}
+            </AnimatePresence>
         </>
     );
 }
---
+++ utils/animations/global.ts
@@ -0,0 +1,5 @@
+export const popDown = {
+    hidden: { opacity: 0, scale: 0.75, x: 0, y: -50 },
+    enter: { opacity: 1, scale: 1, x: 0, y: 0 },
+    exit: { opacity: 0, scale: 0.75, x: 0, y: -50 },
+}
