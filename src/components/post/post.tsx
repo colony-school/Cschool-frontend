c60902f9ec9506d0936fc3453d570d50f8c5a63a
SiravitPhokeed <siravitsasa@gmail.com>
2021-10-30 20:50:13 +0700

    Cleaned up Post imports

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,9 +1,16 @@
-import ReactMarkdown from "react-markdown";
-import { post } from "../../../utils/types"
+// External modules import
 import Image from "next/image";
-import markdownStyles from "../../styles/utils/markdown.module.scss";
-import styles from "../../styles/components/post/post.module.scss";
+import ReactMarkdown from "react-markdown";
+
+// Internal modules import
+import { post } from "../../../utils/types";
+
+// Icons import
 import { ciAnnouncement, ciAttachedFile } from "../../../public/icons";
+
+// Style sheets import
+import styles from "../../styles/components/post/post.module.scss";
+import markdownStyles from "../../styles/utils/markdown.module.scss";
 
 export const AnnouncementPost = ({ post }: { post: post.AnnouncementPost }) => {
     return (
